e33dd2bb9e31661c092167cb9f49c010e0f8fdbd
Naveenravi07 <gamerkidnav@gmail.com>
2025-06-19 13:31:41 +0530

    Added basic files

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
-import { log } from "@repo/logger";
 import { createServer } from "./server";
+import {logger} from "@repo/logger"
 
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-  log(`api running on ${port}`);
+    logger.info("API Gateway started")
 });
--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,15 @@
-export const log = (...args: unknown[]): void => {
-  // eslint-disable-next-line no-console -- logger
-  console.log("LOGGER: ", ...args);
-};
+import pino from "pino";
+
+const logger = pino({
+    level: "info",
+    transport: {
+        target: "pino-pretty",
+        options: {
+            levelFirst: true,
+            translateTime: true,
+            colorize: true,
+        },
+    },
+})
+
+export {logger}
